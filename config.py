2a7142df53374311bf8cf05cae3de800f7b9ccd9
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-02-26 14:14:20 +0100

    update production config

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ if os.environ.get('SERVER_SOFTWARE', '').startswith('Dev'):
         end_point="http://localhost:9090/api/v1/")
 else:
     jerry = dict(
-        key="agxkZXZ-ai1lcnJpY29yDwsSCUFwcEFjY2VzcxgGDA",
-        secret="816cd19005e54c3cafb4a6b31b915ad1")
+        key="agpzfmotZXJyaWNvchALEglBcHBBY2Nlc3MY6QcM",
+        secret="45442a333c924ccbac7d66455e8d9014")
 
